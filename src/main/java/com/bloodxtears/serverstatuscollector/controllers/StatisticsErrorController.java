2a7a29e45b5fec7bc12353ceccdef0905076be64
elasparks <danilka232001@gmail.com>
2022-05-22 22:50:48 +0700

    Add '@Controller' annotation to the ErrorController

--- src/main/java/com/bloodxtears/serverstatuscollector/controllers/StatisticsErrorController.java
+++ src/main/java/com/bloodxtears/serverstatuscollector/controllers/StatisticsErrorController.java
@@ -1,8 +1,10 @@
 package com.bloodxtears.serverstatuscollector.controllers;
 
 import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller
 public class StatisticsErrorController implements ErrorController {
 
     @RequestMapping("/error")
