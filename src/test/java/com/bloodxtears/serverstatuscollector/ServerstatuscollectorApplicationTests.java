ffa75ec2e74545e5ec4ca80b4babce92d8b130e5
elasparks <danilka232001@gmail.com>
2022-05-22 21:20:08 +0700

    Initialize project

---
+++ src/test/java/com/bloodxtears/serverstatuscollector/ServerstatuscollectorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bloodxtears.serverstatuscollector;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerstatuscollectorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
